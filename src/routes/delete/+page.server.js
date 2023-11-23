e8b6109644144b20e731a5f85dc01faca301f819
suvrowebthree <suvro.ghosh.contact@gmail.com>
2023-11-23 18:48:55 +0530

    Refactor database connection and update/delete user functionality

--- src/routes/delete/+page.server.js
+++ src/routes/delete/+page.server.js
@@ -1,74 +1,65 @@
-import { createPool } from '@vercel/postgres';
-import {SECRET_POSTGRES_STRING} from '$env/static/private';
+import { createPool } from "@vercel/postgres";
+import { POSTGRES_URL } from "$env/static/private";
 
 export const config = {
-    runtime: 'nodejs18.x',
+  runtime: "nodejs18.x",
+};
+
+export async function load({}) {
+  const pool = createPool({
+    connectionString: POSTGRES_URL,
+  });
+  //attempt to get users from user table return nothing if table does not exist or error
+  const { rows } = await pool.sql`SELECT * FROM users`;
+  console.log(rows);
+  return { users: rows };
 }
-
-
-export async function load({}){
-    const pool = createPool({
-        connectionString: SECRET_POSTGRES_STRING,
-      });
-    //attempt to get users from user table return nothing if table does not exist or error
-    const {rows} = await pool.sql`SELECT * FROM users`
-    console.log(rows)
-    return {users:rows}
-}
-
 
 async function deleteUser(user) {
-    console.log('user',user);
-    const pool = createPool({
-        connectionString: SECRET_POSTGRES_STRING,
-      });
+  console.log("user", user);
+  const pool = createPool({
+    connectionString: SECRET_POSTGRES_STRING,
+  });
 
-      //delete the user
-      const { result } = await pool.sql`
+  //delete the user
+  const { result } = await pool.sql`
       DELETE FROM users
       WHERE id = ${user.id};
     `;
 
-    return {
-      result
-    };
-  }
-
-
-export const actions={
-    delete: async ({request})=>{
-        
-        const form = await request.formData();
-        const id = form.get('id')??'';
-
-        let deleteResult = false;
-    
-        const data = {
-            id,
-        };
-
-        let deleteResponse = {
-            error:false,
-            message: ''
-        }
-    
-        try{
-            //delete the user
-            const result = await deleteUser(data);
-            console.log('api request ran');
-            if(result) deleteResult = true;
-
-        }catch(err){
-            console.log('api request errored');
-            console.log(err);
-            deleteResponse.error = true;
-            deleteResponse.message = err.message;
-
-        }
-
-            finally{
-                return deleteResponse;
-            }
-    
-    }
+  return {
+    result,
+  };
 }
+
+export const actions = {
+  delete: async ({ request }) => {
+    const form = await request.formData();
+    const id = form.get("id") ?? "";
+
+    let deleteResult = false;
+
+    const data = {
+      id,
+    };
+
+    let deleteResponse = {
+      error: false,
+      message: "",
+    };
+
+    try {
+      //delete the user
+      const result = await deleteUser(data);
+      console.log("api request ran");
+      if (result) deleteResult = true;
+    } catch (err) {
+      console.log("api request errored");
+      console.log(err);
+      deleteResponse.error = true;
+      deleteResponse.message = err.message;
+    } finally {
+      return deleteResponse;
+    }
+  },
+};
